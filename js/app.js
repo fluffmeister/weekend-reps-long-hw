5f229b9b6f8853ce19a5531c574337c0032a9135
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-22 16:42:41 -0700

    forgot to save

--- js/app.js
+++ js/app.js
@@ -1,1 +1,24 @@
 console.log('testing')
+/*I. Variables & Datatypes
+1. How do we assign a value to a variable?
+by using the =
+var=0
+2. How do we change the value of a variable?
+var=1
+var=2
+3. How do we assign an existing variable to a new variable?
+assigning the new variable with the old
+newVar= var
+4. Remind me, what are `declare`, `assign`, and `define`?
+var let const are examples of declaring a variable for the first time(existence)
+assign is the action of storing data to the declared variable
+define is 
+
+5. What is pseudocoding and why should you do it?
+pseduocoding is using comments to better organize your thoughts and list the needed steps to achieve the desired results
+
+6. What percentage of time should be spent thinking about how you're going to solve a problem vs actually typing in code to solve it?
+75-90% planning, and 10-25% actually typing code.
+
+*/
+
