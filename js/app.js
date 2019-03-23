a55761ca97e952eb472b7837290a949cca7a6ce8
Dominic Jaurigue <Domjaurigue@gmail.com>
2019-03-22 20:39:25 -0700

    Project Euler

--- js/app.js
+++ js/app.js
@@ -141,4 +141,16 @@ const sum = bank_account.reduce((accumlator,currentValue)=>{
     return accumlator+currentValue;
 },0);
 console.log(sum)
+
+E. multiples of 3 and 5
 */
+const multiples=[];
+for(let i=0;i<1000;i++){
+    if(i%3==0||i%5==0){
+    multiples.push(i)
+    }
+}
+const sum = multiples.reduce((accumlator,currentValue)=>{
+    return accumlator+currentValue;
+},0);
+console.log(sum)
